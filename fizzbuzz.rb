e3e85af36a459dda7d9baf138843b0ca57f19218
jli063 <jli063@rsdmo.org>
2018-06-19 18:39:43 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,16 +4,17 @@
 
 def fizzbuzz(x)
   # if a number is divisible by 3 and 5, return "fizzbuzz"
-  elsif x%3 == 0 && x%5 == 0 
+  if x%3 == 0 && x%5 == 0 
   puts "fizzbuzz"
   
   # if a number is divisible by 3, return "fizz"
-  if x%3 == 0
+  elsif x%3 == 0
   puts "fizz"
   
   # if a number is divisible by 5, return "buzz"
   elsif x%5 == 0
   puts "buzz"
+  
   end
 end
 
