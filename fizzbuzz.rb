2d186a9c5709eca21327f0ed37cf8fd4e00d288b
jli063 <jli063@rsdmo.org>
2018-06-19 18:43:50 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,21 @@
+
+
+
+
+def fizzbuzz(x)
+  # if a number is divisible by 3 and 5, return "fizzbuzz"
+  if x%3 == 0 && x%5 == 0 
+  puts "fizzbuzz"
+  
+  # if a number is divisible by 3, return "fizz"
+  elsif x%3 == 0
+  puts "fizz"
+  
+  # if a number is divisible by 5, return "buzz"
+  elsif x%5 == 0
+  puts "buzz"
+  
+  end
+end
+
+fizzbuzz(10)
